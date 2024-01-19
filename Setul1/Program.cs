b3cbaf362534e1ed089623fd03e0f6cab5cb5157
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:01:00 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int an;
+            Console.Write("Introduceti anul:");
+            an = int.Parse(Console.ReadLine());
+            if (an % 4 == 0 && an % 100 != 0 || an % 400 == 0)            
+                Console.WriteLine("an bisect");                        
+            else            
+                Console.WriteLine("an normal");            
+        }
+    }
+}
