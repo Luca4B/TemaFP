a67213c4139eefd26d471c0ac5423a756eed347b
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:17:10 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introdu cele 3 numere:");
+            int a=int.Parse(Console.ReadLine());
+            int b=int.Parse(Console.ReadLine());
+            int c=int.Parse(Console.ReadLine());
+            int temp;
+            if (a > b)
+            {  
+                temp = a; 
+                a = b;
+                b = temp;
+            }
+            if(b > c)
+            {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+            if(a > b)
+            {
+                temp = a;
+                a = b;
+                b = temp; 
+            }
+            Console.WriteLine($"numerele in ordine crescatoare sunt: {a}, {b}, {c}");
+        }
+    }
+}
