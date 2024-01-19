8c8c9d058df00561b5c58310c64a9ce96154026c
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:14:53 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce y1: ");
+            int y1 = int.Parse(Console.ReadLine());
+            Console.Write("Introduce y2: ");
+            int y2 = int.Parse(Console.ReadLine());
+            int contor = 0;
+            for(int i = y1;i<= y2;i++)
+            {
+                if (i % 4 == 0 && i % 100 != 0)
+                    contor++;
+                else if (i % 400 == 0)
+                    contor++;
+            }
+            Console.WriteLine($"Intre anii {y1} si {y2} se afla {contor} ani bisecti");
+        }
+    }
+}
