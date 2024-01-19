08c551cd8ec8f8c5d1af57c8b342d9e4bf003558
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 23:29:16 +0200

    Add files via upload

---
+++ Setul3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu pe n");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[n];
+            Console.WriteLine("Introduceti numerele");
+            for (int i = 0; i < n; i++)
+                vector[i] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introdu pe k");
+            int k = int.Parse(Console.ReadLine());
+
+            bool ok = false; 
+            int st = 0, dr = n;
+            int mij, poz = -1;
+            while(st <= dr && ok == false)
+            {
+                mij = (st + dr) / 2;
+                if (k == vector[mij])
+                {
+                    poz = mij;
+                    ok = true;
+                }
+                else
+                    if (k < vector[mij])
+                    dr = mij - 1;
+                else
+                    st = mij + 1;
+            }
+
+            if (ok)
+                Console.WriteLine($"Pozitia elementului k este: {poz}");
+            else
+                Console.WriteLine("Pozitia elementului k este: -1");
+        }
+    }
+}
