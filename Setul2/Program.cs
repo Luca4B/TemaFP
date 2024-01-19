e9f8c3f15757a147ff2c6e568b2a1cb4e5ef0e0d
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:29:26 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            int cnt = 0;
+            for(int i=1; i <= n; i++)
+            {
+                int d = int.Parse(Console.ReadLine());
+                if (d % 2 == 0)
+                    cnt++;
+            }
+            Console.WriteLine($"In aceasta secventa se afla {cnt} numere pare");
+        }
+    }
+}
