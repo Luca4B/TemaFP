22d0c10f55de4f4a4e96de8cc69cb5b8382ef1e8
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:30:04 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n= int.Parse(Console.ReadLine());
+            int poz = 0;
+            int neg = 0;
+            int zero = 0;
+            for(int i = 1;i <= n;i++)
+            {
+                int d = int.Parse(Console.ReadLine());
+                if (d < 0)
+                    neg++;
+                else if (d > 0)
+                    poz++;
+                else zero++;
+            }
+            Console.WriteLine($"Sunt: {neg} negative, {zero} egale cu zero, {poz} pozitive");
+        }
+    }
+}
