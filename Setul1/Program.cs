1fe53d1fa98384e1fdbfdcc594f07f04e2401296
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 20:58:44 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("k = ");
+            int k = int.Parse(Console.ReadLine());
+
+            if (n % k == 0)
+                Console.WriteLine($"{n} se divide cu {k}");
+            else
+                Console.WriteLine($"{n} nu se divide cu {k}");
+        }
+    }
+}
