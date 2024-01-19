d4074916c43c1b1b2327304548f79de90844159b
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:12:48 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.Write("Introdu numarul: ");
+            n=int.Parse(Console.ReadLine());
+            while(n>0)
+            {
+                Console.Write(n % 10);
+                n /= 10;
+            }
+            Console.WriteLine();
+        }
+    }
+}
