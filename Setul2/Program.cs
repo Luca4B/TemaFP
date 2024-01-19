b0222ce73fc71c2e67e8cbffa80c0089a384dfe1
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:36:54 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            int x = 0;
+            int y = 1;
+            int z = 0;
+            for(int i = 3;i<= n;i++)
+            {
+                z = x + y;
+                x = y;
+                y= z;
+            }
+            Console.WriteLine($"Al {n}-lea termen este {z}");
+        }
+    }
+}
