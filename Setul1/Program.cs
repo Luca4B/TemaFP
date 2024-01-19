3ef3a3e520dcb2f1ee87114e2fdedb4e3d4ac7d7
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:08:00 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write("INtrodu pe a: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Introdu pe b: ");
+            b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"a = {a} ; b = {b}");
+        }
+    }
+}
