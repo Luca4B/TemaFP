9b52682a2b857f2431ecb141b48006be909bfabb
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:31:06 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            int produs = 1;
+            for(int i = 1; i <= n; i++)
+            {
+                suma += i;
+                produs *= i;
+            }
+            Console.WriteLine($"suma = {suma} ; prosus = {produs}");
+        }
+    }
+}
