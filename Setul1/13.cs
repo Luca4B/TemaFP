62196fc408b05fe2346cfc390f62f8072e42eda0
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:16:07 +0200

    Update and rename Program.cs to 13.cs

--- Setul1/13.cs
+++ Setul1/13.cs
@@ -17,10 +17,8 @@ namespace _13
             int contor = 0;
             for(int i = y1;i<= y2;i++)
             {
-                if (i % 4 == 0 && i % 100 != 0)
-                    contor++;
-                else if (i % 400 == 0)
-                    contor++;
+                if (i % 4 == 0 && i % 100 != 0 || i % 400 == 0)
+                    contor++;                                    
             }
             Console.WriteLine($"Intre anii {y1} si {y2} se afla {contor} ani bisecti");
         }
