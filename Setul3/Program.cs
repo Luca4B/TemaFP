fcbf5d4662e6e51a659b6279d41792be0df5d0bf
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 23:31:23 +0200

    Add files via upload

---
+++ Setul3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu pe n");
+            int lungime = int.Parse(Console.ReadLine());
+
+            int[] vector = new int[lungime];
+            Console.WriteLine("Introduceti numerele");
+            for (int i = 0; i < lungime; i++)
+                vector[i] = int.Parse(Console.ReadLine());
+
+            int cnt = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                bool diferitZero = true;
+                if (vector[i] == 0)                
+                    diferitZero = false;                
+                if (diferitZero)
+                {
+                    int aux = vector[cnt];
+                    vector[cnt] = vector[i];
+                    vector[i] = aux;
+                    cnt++;
+
+                }
+            }
+
+            Console.WriteLine("Vectorul dupa modificare");
+            foreach (int element in vector)
+                Console.Write(element + " ");
+            Console.WriteLine();
+        }
+    }
+}
