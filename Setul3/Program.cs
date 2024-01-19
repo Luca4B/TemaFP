da6d8e2ab0c4b5259e90201063da4152c3a3f535
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 23:25:10 +0200

    Add files via upload

---
+++ Setul3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu pe n");
+            int n = int.Parse(Console.ReadLine());            
+            int[] vector = new int[n];
+            
+            Console.WriteLine("Introduceti numerele");
+            for (int i = 0; i < n; i++)
+                vector[i] = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Introdu pe k");
+            int k = int.Parse(Console.ReadLine());
+            
+            for (int i = k - 1; i < vector.Length - 1; i++)            
+                vector[i] = vector[i + 1];            
+
+            Array.Resize(ref vector, n - 1);
+
+            Console.WriteLine("Noul vector este");            
+            for(int i = 0;i < vector.Length; i++)
+                Console.WriteLine(vector[i] + " ");
+        }
+    }
+}
