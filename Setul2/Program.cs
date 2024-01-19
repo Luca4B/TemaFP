82c0a7df667e8d70dcf1476338df4102e185ffc3
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:35:04 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu numerele");
+            int d1 = int.Parse(Console.ReadLine());
+            bool ok = true;
+            for(int i = 1; i < n; i++)
+            {
+                int d = int.Parse(Console.ReadLine());
+                if(d1 >= d)
+                    ok = false;
+                d1 = d;
+            }
+            if (ok)
+                Console.WriteLine("Numerele sunt in ordine crescatoare");
+            else
+                Console.WriteLine("Numerele NU sunt in ordine crescatoare");
+        }
+    }
+}
