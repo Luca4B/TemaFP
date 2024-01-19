321fd0ae1f36c1e933b9e964a27bf2b5c2b5916a
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:40:46 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu numerele");
+            int d1 = int.Parse(Console.ReadLine());
+            int scurrent = 1;
+            int smax = 1;
+            for(int i = 0; i < n; i++)
+            {
+                int d = int.Parse(Console.ReadLine());
+                if (d == d1)
+                    scurrent++;
+                else
+                {
+                    if (scurrent > smax)
+                        smax = scurrent;
+                    scurrent = 1;
+                }
+                d1 = d;
+            }
+            Console.WriteLine($"Numarul maxim de numere consecutive egale este: {smax}");
+        }
+    }
+}
