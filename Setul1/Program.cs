26900ea2d971afcc66c002be0760fc2db3c9d7fe
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 22:38:19 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecuatia_gr_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti coeficientii ecuatie de gradul 2.");
+
+            Console.Write("a = ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("b = ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("c = ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            double delta = (b * b) - (4 * a * c);
+
+            if (a != 0)
+                if (delta > 0)
+                {
+                    double x1 = (-b + Math.Sqrt(delta) / (2 * a));
+                    double x2 = (-b - Math.Sqrt(delta) / (2 * a));
+
+                    Console.WriteLine($"Ecuatia are doua radacini reale distincte: {x1} si {x2}");
+                }
+                else if (delta == 0)
+                {
+                    double x = (-b) / (2 * a);
+
+                    Console.WriteLine($"Ecuatia are o singura radacina reala: {x}");
+                }
+                else
+                {
+                    Console.WriteLine("Ecuatia nu are radacini reale.");
+                }
+            else if (a == 0)
+            {
+                if (b == 0)
+                    Console.WriteLine("Numere nu sunt bine introduse.");
+                else
+                    Console.WriteLine($"Ecuatia are o singura radacina: {(-c) / b}.");
+            }
+        }
+    }
+}
