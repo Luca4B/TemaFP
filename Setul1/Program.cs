a593be8faf9d02e5a0859e4dcea127618c2be8d9
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:04:04 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.Write("Introduceti numarul: ");
+            x=int.Parse(Console.ReadLine());
+            int k;
+            Console.Write("Introduceti k: ");
+            k = int.Parse(Console.ReadLine());
+            int i = 1;
+            while(i<k)
+            {
+                x /= 10;
+                i++;
+            }
+            Console.WriteLine($"A {k}-a cifra a numarului este: {x % 10}");
+        }
+    }
+}
