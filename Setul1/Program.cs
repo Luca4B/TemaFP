35307eb186971d7a5481a430ef05d3661ac9a9ba
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:05:13 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            Console.Write("Introdu pe a: ");
+            a=int.Parse(Console.ReadLine());
+            Console.Write("Introdu pe b: ");
+            b=int.Parse(Console.ReadLine());
+            Console.Write("Introdu pe c: ");
+            c=int.Parse(Console.ReadLine());
+            if (a < b + c && b < a + c && c < b + c) 
+                Console.WriteLine("Laturile pot forma un triunghi.");
+            else
+                Console.WriteLine("Laturile nu pot forma un triunghi.");
+        }
+    }
+}
