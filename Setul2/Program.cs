73f2c5b6b31e400f5382e5ae2f80adf1ef529c81
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:36:11 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introdu numerele:");
+            int d = int.Parse(Console.ReadLine());
+            int max = d;
+            int min = d;
+            for(int i = 1; i < n; i++)
+            {
+                d = int.Parse(Console.ReadLine());
+                if (d > max) 
+                    max = d;
+                if(d < min)
+                    min = d;
+            }
+            Console.WriteLine($"cea mai mica valoare: {min} ; cea mai mare valoare: {max}");
+        }
+    }
+}
