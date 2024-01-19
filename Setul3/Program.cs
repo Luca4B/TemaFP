a7a85d39954ad6c140f182e591c26d93dd18c583
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 22:40:13 +0200

    Add files via upload

---
+++ Setul3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu pe n:");
+            int n = int.Parse(Console.ReadLine());
+            int suma = 0;
+            int[] vector = new int[n];
+            Console.WriteLine("Introduceti numerele");
+            for(int i = 0; i < n; i++)            
+                vector[i] = int.Parse(Console.ReadLine());
+            for (int i = 0; i < vector.Length; i++)
+                suma += vector[i];
+            Console.WriteLine($"Suma elementelor vectorului este: {suma}");
+        }
+    }
+}
