cc7a936168e10a8d2da7ad8885b14d813afafc88
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:33:50 +0200

    Add files via upload

---
+++ Setul2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu pe n: ");
+            int n = int.Parse(Console.ReadLine());
+            int cnt=0;
+            for(int i = 0; i < n; i++)
+            {
+                int d = int .Parse(Console.ReadLine());
+                if (d == i)
+                    cnt++;
+            }
+            Console.WriteLine($"Sunt {cnt} elemente care sunt egale cu pozitia lor");
+        }
+    }
+}
