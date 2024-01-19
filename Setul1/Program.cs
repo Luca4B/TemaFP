36024c0f61675fa5840858df5979009c61b72242
Luca Bulea <146916628+Luca4B@users.noreply.github.com>
2024-01-19 21:16:27 +0200

    Add files via upload

---
+++ Setul1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introdu pe n: ");
+            int n=int.Parse(Console.ReadLine());
+            int ogl=0;
+            int c = n;
+            while(c>0)
+            {
+                ogl = ogl * 10 + c%10;
+                c/=10;
+            }
+            if (n == ogl)
+                Console.WriteLine($"Numarul {n} este palindrom");
+            else
+                Console.WriteLine($"Numarul nu {n} este palindrom");
+
+        }
+    }
+}
